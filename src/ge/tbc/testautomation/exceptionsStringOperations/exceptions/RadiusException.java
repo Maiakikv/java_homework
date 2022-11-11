d32bc1960e5d897537ed0a4d577bc51292365af1
Maia Kikvadze <107877382+Maiakikv@users.noreply.github.com>
2022-11-11 19:18:07 +0400

    Delete src/ge/tbc/testautomation directory

--- src/ge/tbc/testautomation/exceptionsStringOperations/exceptions/RadiusException.java
+++
@@ -1,7 +0,0 @@
-package ge.tbc.testautomation.exceptionsStringOperations.exceptions;
-
-public class RadiusException extends RuntimeException{
-    public RadiusException(String message) {
-        super(message);
-    }
-}
