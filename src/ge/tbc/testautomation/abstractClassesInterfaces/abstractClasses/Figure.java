df8a5baff99ae79920d882253bff2939c5accb04
Maia Kikvadze <107877382+Maiakikv@users.noreply.github.com>
2022-11-16 22:09:13 +0400

    Delete src/ge/tbc/testautomation/abstractClassesInterfaces directory

--- src/ge/tbc/testautomation/abstractClassesInterfaces/abstractClasses/Figure.java
+++
@@ -1,7 +0,0 @@
-package ge.tbc.testautomation.abstractClassesInterfaces.abstractClasses;
-
-public abstract class Figure {
-    public abstract double getArea();
-    public abstract double getPerimeter();
-    public abstract void printPackageName();
-}
