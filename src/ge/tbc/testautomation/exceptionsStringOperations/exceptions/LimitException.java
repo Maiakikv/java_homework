39733c27849c87a745baccb5e8c37375855fb3f7
Maia Kikvadze <107877382+Maiakikv@users.noreply.github.com>
2022-11-19 09:35:34 +0400

    a

--- src/ge/tbc/testautomation/exceptionsStringOperations/exceptions/LimitException.java
+++
@@ -1,7 +0,0 @@
-package ge.tbc.testautomation.exceptionsStringOperations.exceptions;
-
-public class LimitException extends RuntimeException{
-    public LimitException(String message) {
-        super(message);
-    }
-}
--- src/ge/tbc/testautomation/exceptionsStringOperations/figures/Figure.java
+++
@@ -1,9 +0,0 @@
-package ge.tbc.testautomation.exceptionsStringOperations.figures;
-
-public class Figure {
-    public static int numberOfInstances;
-
-    public Figure() {
-        numberOfInstances ++;
-    }
-}
