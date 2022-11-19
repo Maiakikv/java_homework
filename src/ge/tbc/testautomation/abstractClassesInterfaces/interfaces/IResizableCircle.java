379b1b66a8aefeebdbb7a24c440089935179a5ce
Maia Kikvadze <107877382+Maiakikv@users.noreply.github.com>
2022-11-19 09:35:47 +0400

    a

--- src/ge/tbc/testautomation/abstractClassesInterfaces/interfaces/IResizableCircle.java
+++
@@ -1,7 +0,0 @@
-package ge.tbc.testautomation.abstractClassesInterfaces.interfaces;
-import ge.tbc.testautomation.abstractClassesInterfaces.figures.Circle;
-
-public interface IResizableCircle {
-    Circle returnDoubleSizedCircle(Circle circle);
-    Circle returnCustomSizedCircle(Circle circle, double byvValue);
-}
--- src/ge/tbc/testautomation/abstractClassesInterfaces/interfaces/IValidCircle.java
+++
@@ -1,7 +0,0 @@
-package ge.tbc.testautomation.abstractClassesInterfaces.interfaces;
-
-import ge.tbc.testautomation.abstractClassesInterfaces.figures.Circle;
-
-public interface IValidCircle {
-    boolean validateCircle(Circle circle);
-}
